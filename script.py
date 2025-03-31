03aa6b4aa3551eb677b3aa211c21d759ba9eef48
Dereck368 <derecktaverne@gmail.com>
2025-03-31 00:45:22 -0400

    fixed the program, connections are a bit inconsistent, running another test

--- script.py
+++ script.py
@@ -10,33 +10,16 @@ import youtubeAPI
             #How IN THE WORLD DO I ACCESS SOMEONE ELSE WATCH LATER
             #selenium
 
-#TODO WHY IS IT NOT SENDING ??? !!!! : <<< (((
-#TODO can you edit a constant stat DONE in main from other classes that
-    # import it 
-
-async def send_playlist_information(msg, client_tup, pipe):
-    status = msg.decode('utf-8')
-    print(status)
-    
+#TODO Incositent connections.
 
 async def create_foreign_playlist(msg, client_tup, pipe):
-    
-    # if not os.path.exists('Playlist Hashes.txt'):
-
-    # playlist_json = msg.decode('utf-8')
-    # print(playlist_json)
-    # if decoded an not json string know to do nothing
     try:
         playlist_json = msg.decode('utf-8')
-        json.loads(playlist_json)  # Try to parse the string as JSON\
+        json.loads(playlist_json)  
         print("recieved")
-        reply = "PlayList sent SUCCESFULLY, shutting down now"
         # youtubeAPI.createPlaylist(playlist_json)
-        status = reply.encode('utf-8')
-        await pipe.send(status)
     except json.JSONDecodeError:
         pass
-        # await pipe.send(reply)  # Not a valid JSON string
 
 async def main():
     node = None
@@ -83,10 +66,7 @@ async def main():
                     except:
                         print("Server error")
 
-                    # waiting need to implement
-                    # set variable as turn off signal
-                    # Exit
-                    print("Waiting")
+                    print("If you don't see a msg of P2P... you are not connected. restart process.")
                     while not youtubeAPI.isDone():
                         await asyncio.sleep(1)
 
@@ -96,7 +76,6 @@ async def main():
                     sys.exit()
 
             if choice == "2":
-                node.add_msg_cb(send_playlist_information)
                 await node.start(out=True) # this prints the process of node starting
                 await node.nickname(node.node_id)
                 option = ""
@@ -167,66 +146,12 @@ async def main():
 
                                     id = playlist_ids[playlist]
 
-                                    #TODO fix binary data
-                                    # says Trying to write to closed socket
-
-                                    '''
-                                    Split the files into blocks let's say of 100KB each. 
-                                    Then calculate a SHA hash (or some other hashing algorithm) on each of the blocks. 
-                                    so if the file is 905KB, you would have 10 such hashes calculated.
-
-                                    The server would contain a hash definition file for each file that it serves. 
-                                    This hash definition file would contain a list of all of the blocks of the file, along with the hash. 
-                                    So if the server is serving our 905KB file called test.exe. 
-                                    Then we would have another file called test.exe.hashes which contains a listing of the 10 hashes of the file.
-
-                                    The client would download the hash definition file, and ensure that it has all of the blocks. 
-                                    The client can request each block individually and after it's downloaded, it can calculate the hash on its end again to ensure there is no corruption.
-
-                                    You don't need to physically split the file, splitting a file is just reading the part of it that you are interested in.   
-                                    The first block of the file is from byte range 0 to 102399, the next block is from 102400 to 204800, and so on. 
-                                    So just open the file, seek to that position, read the data, and close the file.
-                                    '''
-
                                     playlist_json = youtubeAPI.getPlaylistItems(id)
-                                    # size_kb = len(playlist_json) / 1024
 
-                                    # if size_kb <= 100:
-                                        # Add hashing (example, you can customize this part)
                                     binary_data = playlist_json.encode('utf-8')
-                                    # h = "Hello"
-                                    # await pipe.send(h.encode('utf-8'))
+
                                     await pipe.send(binary_data)
                                     
-                                    # Receive response and check if it's successful
-                                    # TODO finish function above for response
-                                            
-                                    # else:
-                                    #     playlist_Hashchunks = []
-                                    #     chunk_size = 100 * 1024  # 100KB in bytes
-                                    #     iterations = math.ceil(size_kb / 100)
-
-                                    #     # Generate chunk slices and send them
-                                    #     for i in range(iterations):
-                                    #         start_idx = i * chunk_size
-                                    #         end_idx = min((i + 1) * chunk_size, len(playlist_json))
-
-                                    #         chunk = playlist_json[start_idx:end_idx]
-                                    #         received_hash = hashlib.sha256(chunk).hexdigest()
-                                    #         playlist_Hashchunks.append(received_hash)
-
-                                    #     byte_data = joined_hashes.encode('utf-8')  # Encode the string into bytes (UTF-8 encoding)
-                                    #     print("Byte Data:", byte_data)
-
-                                    #     # Example of sending this byte data over a pipe (simulated with await statements)
-                                    #     # Assuming you have a `pipe` object available for communication
-
-                                    #     await pipe.send(b"ECHO " + byte_data + b"\n")
-                                    #     # return number of item hashes
-                                    #     # get those
-
-                                    #     playlist_Hashchunks_string = ''.join(playlist_Hashchunks)
-                                    #     binary_string = ''.join(format(ord(char), '08b') for char in playlist_Hashchunks_string)
                                     option = "exit"
                                     await pipe.close()
                                     break
@@ -238,8 +163,6 @@ async def main():
         if choice == "3":
             print("Stopping program...")
             if node:
-                print(".")
-                print("..")
                 await node.close()
             print("Successfully exiting program. Please Wait.")
             sys.exit()
@@ -247,7 +170,7 @@ async def main():
         if choice == "4":
             print("deleting token...")
             authorization.delete_token()
-            print("token deleted...")
+            print("token deleted")
             sys.exit()
             
 
